7ba33c95bef97a47ae75eca817d37ed4168e9b80
Anatolii <115033897+GOSTT22@users.noreply.github.com>
2024-04-01 12:10:49 +0300

    created date

--- models/task.js
+++ models/task.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Task = mongoose.model('Task', { 
     task_name: String,
     description: String,
-    status: String
+    status: String,
+    createdAt: { type: Date, required: true, default: Date.now }
 });
 
 module.exports = Task;
