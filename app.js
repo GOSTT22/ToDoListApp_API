54acf079295598fb8f3c05e04c97382f841cd32b
Anatolii <115033897+GOSTT22@users.noreply.github.com>
2024-04-24 13:16:14 +0300

    login done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require('express-cors')
 const dotenv = require("dotenv");
 dotenv.config();
 //connect
@@ -20,6 +21,11 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(cors({
+  allowedOrigins: [
+      'localhost:4200'
+  ]
+}))
 
 app.use(logger("dev"));
 app.use(express.json());
